689b9b7aeb7b321c5f6248507a78bd2aaaa8b5ea
Alexis <33222808+sathonay@users.noreply.github.com>
2025-10-09 17:43:11 +0200

    add ex04

---
+++ ex04/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <ios>
+#include <fstream>
+#include <string>
+
+std::string str_replace(std::string str, std::string find, std::string replace)
+{
+	size_t f_length = find.length();
+	std::size_t offset = str.find(find);
+	while (offset != std::string::npos)
+	{
+		str.erase(offset, f_length);
+		str.insert(offset, replace);
+		offset = str.find(find);
+	}
+	return (str);
+}
+
+int main(int ac, char **av)
+{
+	if (ac != 4)
+	{
+		std::cout << *av << " file <str_to_replace> <str_replacement>" << std::endl;
+		return 1;
+	}
+	std::string filename(av[1]);
+	std::string find(av[2]);
+	std::string replacement(av[3]);
+
+	std::ifstream infile(filename.c_str(), std::ifstream::in);
+	std::ofstream outfile (filename.append(".replace").c_str(), std::ofstream::trunc);
+
+	std::string line;
+	do {
+		try
+		{
+			std::getline(infile, line);
+			outfile << str_replace(line, find, replacement) << std::endl;
+		} catch (std::ios_base::failure& ex)
+		{
+			std::cout << ex.what() << std::endl;
+			break;
+		}
+//		https://cplusplus.com/reference/ios/basic_ios/rdstate
+	} while (infile.rdstate() == 0 && outfile.rdstate() == 0);
+
+	if ((infile.rdstate() & std::ifstream::failbit) && !(infile.rdstate() & std::ifstream::eofbit))
+		std::cerr << "Infile: Logical error on i/o operation" << std::endl;
+	if ((infile.rdstate() & std::ifstream::badbit) && !(infile.rdstate() & std::ifstream::eofbit))
+		std::cerr << "Infile: Read/writing error on i/o operation" << std::endl;
+
+	if ((outfile.rdstate() & std::ifstream::failbit) && !(outfile.rdstate() & std::ifstream::eofbit))
+		std::cerr << "Outfile: Logical error on i/o operation" << std::endl;
+	if ((outfile.rdstate() & std::ifstream::badbit) && !(outfile.rdstate() & std::ifstream::eofbit))
+		std::cerr << "Outfile: Read/writing error on i/o operation" << std::endl;
+
+}
