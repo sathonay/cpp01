7136bbee3a9d1f7ae79fe2fbf5be50ba5dc7574f
Alexis <33222808+sathonay@users.noreply.github.com>
2025-12-04 16:03:47 +0100

    fix messages

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -3,17 +3,17 @@
 
 void Harl::debug( void )
 {
-	std::cout << "love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!"	<< std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!"	<< std::endl;
 }
 
 void Harl::info( void )
 {
-	std::cout << " cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void Harl::warning( void )
 {
-	std::cout << " think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
 }
 
 void Harl::error( void )
